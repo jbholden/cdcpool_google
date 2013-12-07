be70517d5892d753eb1664d7efabaadc96669032
Brent Holden <Brent@brent-holdens-macbook-pro-2.local>
2013-12-07 15:54:57 -0500

    added code for secure cookies and json

--- pages/handler.py
+++ pages/handler.py
@@ -1,6 +1,8 @@
 import webapp2 
 import jinja2 
 import os
+import json
+import utils.utils as utils
 
 template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"../views"))
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
@@ -15,3 +17,18 @@ class Handler(webapp2.RequestHandler):
 
     def render(self,template,**kw):
         self.write(self.render_str(template,**kw))
+
+    def render_json(self, d):
+        json_txt = json.dumps(d)
+        self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
+        self.write(json_txt)
+
+    def set_secure_cookie(self, name, val):
+        cookie_val = utils.make_secure_val(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and utils.check_secure_val(cookie_val)
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,17 @@
 # Place to put globally useful utilities.
 
 import os
+import hmac
+
+secret = "cdcpool_abc1234"
 
 def is_debug():
     return os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
+
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val

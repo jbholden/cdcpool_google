7735c844efeb499f41290056553e52318aa8f7b4
Brent Holden <brent.l.holden@gmail.com>
2014-01-02 15:37:44 -0500

    working untested player results

--- pages/player_results_page.py
+++ pages/player_results_page.py
@@ -5,13 +5,16 @@ from code.database import *
 from handler import *
 import string
 import re
+import utils.timezone as tz
 
-# TODO:  move quarter/timeleft column values to this class
 # TODO:  define base player results and fill in blocks with another html
 # TODO: tests
 #       + game not started:  view page with a game start date
 #       + game not started:  view page with a game start date (different time zones)
 #       + game not started:  view page with no game start date
+#       + game in progress:  view page with quarter/time combinations
+#       + game final:  view page with game final
+
 
 class PlayerResultsPage(Handler):
 
@@ -25,17 +28,35 @@ class PlayerResultsPage(Handler):
         players_in_year = database.load_players(year)
         return str(player_key) in players_in_year
 
-    def __get_top_row_game_status(self,result):
-        # game not started
-        # + no start date
-        # + start date
-        # game in progress
-        # game final
-        pass
+    def set_game_status_params(self,params,results):
+        top_ids = []
+        top_statuses = []
+        bottom_ids = []
+        bottom_statuses = []
+
+        for result in results:
+            top_status,bottom_status,top_id,bottom_id = self.get_game_status(result)
+
+            top_ids.append(top_id)
+            top_statuses.append(top_status)
+            bottom_ids.append(bottom_id)
+            bottom_statuses.append(bottom_status)
+
+        params['top_status_id'] = top_ids
+        params['top_status'] = top_statuses
+        params['bottom_status_id'] = bottom_ids
+        params['bottom_status'] = bottom_statuses
+
 
     def get_game_status(self,result):
         if result.game_state == "not_started":
             return self.__game_not_started_status(result)
+        elif result.game_state == "in_progress":
+            return self.__game_in_progress_status(result)
+        elif result.game_state == "final":
+            return self.__game_final_status()
+        raise AssertionError,"bad game state: %s" % (result.game_state)
+
 
     def __game_not_started_status(self,result):
         if result.game_date == None:
@@ -44,13 +65,67 @@ class PlayerResultsPage(Handler):
             top_id = "status-empty"
             bottom_id = "status-empty"
         else:
+            local_game_date = result.game_date.astimezone(self.__get_local_timezone())
             weekday_month_day = "%a %b %d"
             hour_minutes_ampm_timezone = "%I:%M %p %Z"
-            top_status = result.game_date.strftime(weekday_month_day)
-            bottom_status = result.game_date.strftime(hour_minutes_ampm_timezone)
+            top_status = local_game_date.strftime(weekday_month_day)
+            bottom_status = local_game_date.strftime(hour_minutes_ampm_timezone)
             top_id = "game-time"
             bottom_id = "game-time"
         return top_status,bottom_status,top_id,bottom_id
+
+    def __game_in_progress_status(self,result):
+        quarter_missing = not(result.game_quarter) or result.game_quarter == ""
+        time_left_missing = not(result.game_time_left) or result.game_time_left == ""
+
+        if quarter_missing and time_left_missing:
+            top_status = ""
+            bottom_status = "in progress"
+            top_id = "status-empty"
+            bottom_id = "game-in-progress"
+        elif quarter_missing and not(time_left_missing):
+            top_status = ""
+            bottom_status = result.game_time_left
+            top_id = "status-empty"
+            bottom_id = "game-time-in-progress"
+        elif not(quarter_missing) and time_left_missing:
+            top_status = ""
+            bottom_status = result.game_quarter
+            top_id = "status-empty"
+            bottom_id = "game-quarter"
+        else:
+            top_status = result.game_quarter 
+            bottom_status = result.game_time_left
+            top_id = "game-quarter"
+            bottom_id = "game-time-in-progress"
+
+        return top_status,bottom_status,top_id,bottom_id
+
+    def __game_final_status(self):
+        top_status = ""
+        bottom_status = "final"
+        top_id = "status-empty"
+        bottom_id = "game-final"
+
+        return top_status,bottom_status,top_id,bottom_id
+
+
+    def __get_local_timezone(self):
+        # TODO:  change this code to player's preferred timezone
+        try:
+            return self.__timezone
+        except AttributeError:
+            return tz.Eastern
+
+    def set_timezone_for_testing(self,timezone):
+        if timezone == "Eastern":
+            self.__timezone = tz.Eastern
+        elif timezone == "Central":
+            self.__timezone = tz.Central
+        elif timezone == "Mountain":
+            self.__timezone = tz.Mountain
+        elif timezone == "Pacific":
+            self.__timezone = tz.Pacific
 
 
     def get(self,year_param,week_number_param,player_id_param):
@@ -85,5 +160,7 @@ class PlayerResultsPage(Handler):
         params['weeks_in_year'] = database.get_week_numbers(year)
         params['summary'] = summary
         params['results'] = results
+
+        self.set_game_status_params(params,results)
 
         self.render("player_results.html",**params)
--- tests/test_player_results.py
+++ tests/test_player_results.py
@@ -14,6 +14,9 @@ import utils.timezone as tz
 class GameStatusTestData:
     date = None
     state = None
+    timezone = None
+    time_left = None
+    quarter = None
     top_id = None
     top_status = None
     bottom_id = None
@@ -27,11 +30,16 @@ class TestPlayerResults(unittest.TestCase):
         self.__timezone_daylight_time_test()
         #self.__timezone_local_time_test()  # this doesn't seem to be working
 
-    def test_t1_not_started_game_status(self):
+    def test_t1_game_status(self):
         self.__t1_game_not_started_status_no_date()
-        self.__t1_game_not_started_status_date()
-        #self.__t1_game_not_started_status_date_est()
-        #self.__t1_game_not_started_status_date_pst()
+        self.__t1_game_not_started_status_date_est()
+        self.__t1_game_not_started_status_date_edt()
+        self.__t1_game_not_started_status_date_pst()
+        self.__t1_game_not_started_status_date_pdt()
+        self.__t1_game_in_progress()
+        self.__t1_game_in_progress_quarter()
+        self.__t1_game_in_progress_time()
+        self.__t1_game_in_progress_quarter_time()
 
     def __timezone_test(self,testdate,expected):
         fmt = "%I:%M %p %Z"
@@ -87,14 +95,113 @@ class TestPlayerResults(unittest.TestCase):
         testdata.bottom_status = ""
         self.__test_game_status(testdata)
 
-    def __t1_game_not_started_status_date(self):
+    def __t1_game_not_started_status_date_est(self):
         testdata = GameStatusTestData()
         testdata.date = datetime.datetime(2010,2,24,19,30,tzinfo=tz.Eastern)
         testdata.state = "not_started"
+        testdata.timezone = "Eastern"
         testdata.top_id = "game-time"
         testdata.top_status = "Wed Feb 24"
         testdata.bottom_id = "game-time"
         testdata.bottom_status = "07:30 PM EST"
+        self.__test_game_status(testdata)
+
+    def __t1_game_not_started_status_date_edt(self):
+        testdata = GameStatusTestData()
+        testdata.date = datetime.datetime(2014,8,24,19,30,tzinfo=tz.Eastern)
+        testdata.state = "not_started"
+        testdata.timezone = "Eastern"
+        testdata.top_id = "game-time"
+        testdata.top_status = "Sun Aug 24"
+        testdata.bottom_id = "game-time"
+        testdata.bottom_status = "07:30 PM EDT"
+        self.__test_game_status(testdata)
+
+    def __t1_game_not_started_status_date_pst(self):
+        testdata = GameStatusTestData()
+        testdata.date = datetime.datetime(2010,2,24,19,30,tzinfo=tz.Eastern)
+        testdata.state = "not_started"
+        testdata.timezone = "Pacific"
+        testdata.top_id = "game-time"
+        testdata.top_status = "Wed Feb 24"
+        testdata.bottom_id = "game-time"
+        testdata.bottom_status = "04:30 PM PST"
+        self.__test_game_status(testdata)
+
+    def __t1_game_not_started_status_date_pdt(self):
+        testdata = GameStatusTestData()
+        testdata.date = datetime.datetime(2014,8,24,19,30,tzinfo=tz.Eastern)
+        testdata.state = "not_started"
+        testdata.timezone = "Pacific"
+        testdata.top_id = "game-time"
+        testdata.top_status = "Sun Aug 24"
+        testdata.bottom_id = "game-time"
+        testdata.bottom_status = "04:30 PM PDT"
+        self.__test_game_status(testdata)
+
+    def __t1_game_in_progress_quarter_time(self):
+        testdata = GameStatusTestData()
+        testdata.date = datetime.datetime(2014,8,24,19,30,tzinfo=tz.Eastern)
+        testdata.state = "in_progress"
+        testdata.timezone = "Eastern"
+        testdata.time_left = "15:00"
+        testdata.quarter = "3rd"
+        testdata.top_id = "game-quarter"
+        testdata.top_status = "3rd"
+        testdata.bottom_id = "game-time-in-progress"
+        testdata.bottom_status = "15:00"
+        self.__test_game_status(testdata)
+
+    def __t1_game_in_progress_quarter(self):
+        testdata = GameStatusTestData()
+        testdata.date = datetime.datetime(2014,8,24,19,30,tzinfo=tz.Eastern)
+        testdata.state = "in_progress"
+        testdata.timezone = "Eastern"
+        testdata.time_left = ""
+        testdata.quarter = "Halftime"
+        testdata.top_id = "status-empty"
+        testdata.top_status = ""
+        testdata.bottom_id = "game-quarter"
+        testdata.bottom_status = "Halftime"
+        self.__test_game_status(testdata)
+
+    def __t1_game_in_progress_time(self):
+        testdata = GameStatusTestData()
+        testdata.date = datetime.datetime(2014,8,24,19,30,tzinfo=tz.Eastern)
+        testdata.state = "in_progress"
+        testdata.timezone = "Eastern"
+        testdata.time_left = "3:25"
+        testdata.quarter = ""
+        testdata.top_id = "status-empty"
+        testdata.top_status = ""
+        testdata.bottom_id = "game-time-in-progress"
+        testdata.bottom_status = "3:25"
+        self.__test_game_status(testdata)
+
+    def __t1_game_in_progress(self):
+        testdata = GameStatusTestData()
+        testdata.date = datetime.datetime(2014,8,24,19,30,tzinfo=tz.Eastern)
+        testdata.state = "in_progress"
+        testdata.timezone = "Eastern"
+        testdata.time_left = ""
+        testdata.quarter = ""
+        testdata.top_id = "status-empty"
+        testdata.top_status = ""
+        testdata.bottom_id = "game-in-progress"
+        testdata.bottom_status = "in progress"
+        self.__test_game_status(testdata)
+
+    def __t1_game_final(self):
+        testdata = GameStatusTestData()
+        testdata.date = datetime.datetime(2014,8,24,19,30,tzinfo=tz.Eastern)
+        testdata.state = "final"
+        testdata.timezone = "Eastern"
+        testdata.time_left = ""
+        testdata.quarter = ""
+        testdata.top_id = "status-empty"
+        testdata.top_status = ""
+        testdata.bottom_id = "game-final"
+        testdata.bottom_status = "final"
         self.__test_game_status(testdata)
 
     def __test_game_status(self,test_data):
@@ -102,6 +209,9 @@ class TestPlayerResults(unittest.TestCase):
         result = PlayerResult()
         result.game_date = test_data.date
         result.game_state = test_data.state
+        result.game_time_left = test_data.time_left
+        result.game_quarter = test_data.quarter
+        page.set_timezone_for_testing(test_data.timezone)
         top_status,bottom_status,top_id,bottom_id = page.get_game_status(result)
         self.assertEqual(top_status,test_data.top_status)
         self.assertEqual(top_id,test_data.top_id)

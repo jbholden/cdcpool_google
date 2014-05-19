3cc687b1ccc4093781e5418a15f93fc3adac42fd
Brent Holden <brent.l.holden@gmail.com>
2014-05-19 16:39:26 -0400

    some get team api code

--- code/api.py
+++ code/api.py
@@ -54,4 +54,34 @@ class API:
 
         d.delete_team_from_memcache(team)
 
+    def get_team(self,name):
+        d = Database()
+        teams = d.load_teams("teamkeys")
+
+        if name not in teams:
+            raise APIException(404,"could not find the team")
+            return
+
+        team = db.get(teams[name])
+        return team
+
+    def get_team_by_key(self,team_key):
+        d = Database()
+        teams = d.load_teams("teams")
+
+        if team_key not in teams:
+            raise APIException(404,"could not find the team")
+            return
+
+        team = db.get(team_key)
+        return team
+
+    def get_team_by_id(self,team_id):
+        try:
+            team_key = db.Key.from_path('Team',team_id)
+        except:
+            raise APIException(500,"exception when getting key")
+            return
+        return self.get_team_by_key(str(team_key))
+
 
--- pages/api/team_api.py
+++ pages/api/team_api.py
@@ -12,11 +12,38 @@ class TeamAPIPage(APIHandler):
     # this gets a team object
     def get(self):
         data = json.loads(self.request.body) 
-             
-        if 'name' not in data: 
+
+        num_params = 0
+        if 'id' in data: 
+            num_params += 1
+        if 'key' in data: 
+            num_params += 1
+        if 'name' in data: 
+            num_params += 1
+
+        if num_params != 0:
             self.error(400) 
-            self.write("user name is missing") 
+            self.write("only one parameter should be defined to find the team")
             return 
+
+        try:
+            api = API()
+            if 'name' in data:
+                team = api.get_team(data['name'])
+            elif 'key' in data:
+                team = api.get_team_by_key(data['key'])
+            elif 'id' in data:
+                team = api.get_team_by_id(data['id'])
+            else:
+                raise AssertionError,"should not get here"
+
+        except APIException as e:
+            self.error(e.http_code)
+            self.write(e.errmsg)
+            return
+
+
+             
 
     # this creates a new team
     def post(self):

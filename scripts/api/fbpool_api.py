7f45c713e32f183a35cf3327eb9d0223546039cc
Brent Holden <brent.l.holden@gmail.com>
2014-05-19 13:39:58 -0400

    team api tests

--- scripts/api/fbpool_api.py
+++ scripts/api/fbpool_api.py
@@ -15,3 +15,37 @@ class FBPoolAPI:
             data = json.loads(response.read())
             return data
         raise FBAPIException(response.code,response.read())
+
+    def deleteTeam(self,name):
+        pass
+
+    def deleteTeamByKey(self,team_key):
+        pass
+
+    def deleteTeamByID(self,team_id):
+        pass
+
+    def getTeam(self,name):
+        pass
+
+    def getTeamByKey(self,team_key):
+        pass
+
+    def getTeamByID(self,team_id):
+        pass
+
+    def getAllTeams(self):
+        pass
+
+    def deleteAllTeams(self):
+        pass
+
+    def deleteTeamIfExists(self,name):
+        pass
+
+    def addTeam(self,name,conference):
+        pass
+
+    def createTeams(self):
+        pass
+
